23c33b6ecd2a5f225c2ee603e74694be7c02e3e9
Arun K. Singh <arunkumar.singh2@ihsmarkit.com>
2019-04-29 15:25:12 +0530

    with global

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var Web3 = require('web3')
+//var Web3 = require('web3')
 var router = express.Router();
 
 /* GET home page. */
@@ -8,23 +8,23 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/health', function(req, res, next) {
-    const web3 = new Web3(new Web3.providers.HttpProvider('http://3.94.188.142:22000'));
-    const web3Provider = new Web3(new Web3.providers.WebsocketProvider("ws://3.94.188.142:22005"));
-    console.log(web3.eth.net.getPeerCount().then(console.log));
-    //console.log(web3Provider);
+    // const web3 = new Web3(new Web3.providers.HttpProvider('http://3.94.188.142:22000'));
+    // const web3Provider = new Web3(new Web3.providers.WebsocketProvider("ws://3.94.188.142:22005"));
+    // console.log(web3.eth.net.getPeerCount().then(console.log));
+    // //console.log(web3Provider);
 
-    //web3.eth.getBlock('pending');
-    web3.eth.isSyncing(function(err, data) {
+    // //web3.eth.getBlock('pending');
+    web3Http.eth.isSyncing(function(err, data) {
         console.log(err + '==' + data);
     });
-    web3.eth.isMining(function(err, data) {
+    web3Http.eth.isMining(function(err, data) {
         console.log(err + '==' + data);
     });
-    web3.eth.getNodeInfo(function(err, data) {
+    web3Http.eth.getNodeInfo(function(err, data) {
         console.log(err + '==' + data);
     });
 
-    const subscription = web3Provider.eth.subscribe('pendingTransactions');
+    const subscription = web3Socket.eth.subscribe('pendingTransactions');
     subscription.on("data", function(transaction) {
         console.log(transaction);
     });
