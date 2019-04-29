51aafa3ccaefe64dce751ba6ca290bb824ab807a
Arun Singh <arunsingh@Aruns-MacBook-Pro.local>
2019-04-29 11:06:32 +0530

    with pendinhg

--- routes/index.js
+++ routes/index.js
@@ -10,29 +10,37 @@ router.get('/', function(req, res, next) {
 router.get('/health', function(req, res, next) {
   const web3  = new Web3(new Web3.providers.HttpProvider('http://3.94.188.142:22000'));
   
-  const web3Provider = new Web3(new Web3.providers.WebsocketProvider("ws://3.94.188.142:22000"));
+  const web3Provider = new Web3(new Web3.providers.WebsocketProvider("ws://3.94.188.142:22005"));
     console.log(web3.eth.net.getPeerCount().then(console.log));
-    //console.log(web3.eth.getPendingTransactions());
+    //console.log(web3Provider);
     
-  const subscription = web3Provider.eth.subscribe('pendingTransactions').then(console.log);
+    //web3.eth.getBlock('pending');
+    web3.eth.isSyncing(function(err,data){
+        console.log(err + '=='+data);
+    });
+    web3.eth.isMining(function(err,data){
+        console.log(err + '=='+data);
+    });
+    web3.eth.getNodeInfo(function(err,data){
+        console.log(err + '=='+data);
+    });
+  
+    const subscription = web3Provider.eth.subscribe('pendingTransactions');
+
+
+
+
   subscription.on("data", function(transaction){
       console.log(transaction);
   });
 
+  subscription.on("error", function(transaction){
+    console.log(transaction);
+});
 
-  const subscription2 = web3Provider.eth.subscribe('newBlockHeaders', function(error, result){
-      if (!error) {
-          console.log(result);
+  
 
-          return;
-      }
-
-      console.error(error);
-  })
-  .on("data", function(blockHeader){
-      console.log(blockHeader);
-  })
-  .on("error", console.error);
+  
 
   res.render('index', { title: 'Express' });
 });
